e9608e783fbf4bb3c44b78b1ce18610960041f9c
Aidan <127414645+caidol@users.noreply.github.com>
2023-07-01 21:41:16 +0100

    Added config files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+
+vim.cmd("set termguicolors")
+vim.cmd("let g:gruvbox_contrast_dark = 'hard'")
+vim.cmd("let g:gruvbox_contrast_light = 'hard'")
+vim.cmd("colorscheme gruvbox")
+vim.cmd("hi LspCxxHlGroupMemberVariable guifg=#83a598")
+
+
