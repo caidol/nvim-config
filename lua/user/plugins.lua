d4260bb994f7cc002bf5f6dc3b8ef127e14713cc
Aidan <127414645+caidol@users.noreply.github.com>
2023-07-02 23:41:56 +0100

    Add files via upload

---
+++ lua/user/plugins.lua
@@ -0,0 +1,45 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+	fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd [[packadd packer.nvim]]
+	return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim' 
+  
+    -- Colorscheme
+    use {'morhetz/gruvbox', config = function() vim.cmd.colorscheme("gruvbox") end }
+  
+    use 'luochen1990/rainbow' 
+
+    -- cmp plugins
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp" -- lsp completions
+
+    -- snippets
+    use "L3MON4D3/LuaSnip" --snippet engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    -- LSP 
+    use "neovim/nvim-lspconfig" -- enable LSP
+    use "williamboman/mason.nvim" -- simple to use language server installer
+    use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+    use "jose-elias-alvarez/null-ls.nvim" -- LSP diagnostics and code actions
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+	require('packer').sync()
+    end
+end)
