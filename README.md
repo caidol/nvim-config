11961c5059ec761e8d7d327dff6fbcf3bc1bd5e4
Aidan <127414645+caidol@users.noreply.github.com>
2023-07-05 23:06:57 +0100

    Updating docs and fix plugins.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nvim-config
+
+This repository is an nvim config which I have been working on. It contains all standard features
+such as LSP, telescope, treesitter and a sleek yet colourful gruvbox colourscheme.
+
+The inspiration for the colourscheme has come from the youtuber jdh, who also has used neovim recently in his videos
+with the same gruvbox colourscheme.
+
+
+Below is a picture showing what the editor looks like (this is upon opening a Python file):
+
+![alt text](https://github.com/caidol/nvim-config/images/editor.png)
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -2,9 +2,9 @@ local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
+fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+vim.cmd [[packadd packer.nvim]]
+return true
   end
   return false
 end
@@ -12,16 +12,50 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim' 
-  
+  use "wbthomason/packer.nvim" 
+  use "nvim-lua/popup.nvim" 
+  use "nvim-lua/plenary.nvim" 
+  use "windwp/nvim-autopairs" 
+
   -- Colorscheme
   use {'morhetz/gruvbox', config = function() vim.cmd.colorscheme("gruvbox") end }
-  
+
   use 'luochen1990/rainbow' 
 
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp" -- lsp completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP 
+  use "neovim/nvim-lspconfig" -- enable LSP 
+  use "williamboman/mason.nvim" -- simple to use language server installer
+  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim" -- Telescope for nvim
+  use "nvim-telescope/telescope-media-files.nvim" -- Telescope extension that supports media files
+  
+  -- Treesitter
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }    
+  use "p00f/nvim-ts-rainbow"
+  
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+require('packer').sync()
   end
 end)
